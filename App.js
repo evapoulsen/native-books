fdcaddf0d37a5b76a12f6f55db77f00543557743
Eva S. Poulsen <eva@btech.au.dk>
2022-04-16 15:28:47 +0200

    Logo in Home Screen

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import { Button } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import BooksScreen from "./src/screen/BooksScreen";
 import AuthorsScreen from './src/screen/AuthorsScreen';
 import HomeScreen from './src/screen/HomeScreen';
+import LogoTitle from './src/screen/LogoTitle';
 
 
 const Stack = createNativeStackNavigator();
@@ -12,15 +14,26 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen 
-        name="Home" 
-        component={HomeScreen} 
-        options={{ title: 'My Home'}}
+      <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            headerLeft: (props) => <LogoTitle {...props} onPress={() => navigation.goBack()}/>,
+            title: 'My Home',
+            headerRight: () => (
+              <Button
+                onPress={() => alert('This is a button!')}
+                title="Info"
+                color="#04B4AE"
+              />
+            ),
+          }}
         />
         <Stack.Screen 
         name="Books" 
         component={BooksScreen}
         options={({ route }) => ({ title: route.params.name })}
+        
         />
         <Stack.Screen 
         name="Authors" 
