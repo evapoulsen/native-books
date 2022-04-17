4975bf7686f7764fd98c06a180c286868bb7f87e
Eva S. Poulsen <eva@btech.au.dk>
2022-04-17 13:41:22 +0200

    Google Fonts

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,14 +1,40 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, FlatList } from "react-native";
+import AppLoading from "expo-app-loading";
+import books from "../../data/books.json";
+import { useFonts, Raleway_200ExtraLight } from "@expo-google-fonts/raleway";
+
 
 const BookList = (props) => {
+    let [fontsLoaded] = useFonts({
+        Raleway_200ExtraLight,
+      });
+      if (!fontsLoaded) {
+          return <AppLoading />
+      }
     return (
         <View>
-            <Text>Book List</Text>
+            <FlatList
+            data={books}
+            renderItem={({ item }) => {
+                return (
+                    <Text style={styles.textStyle}>{item.title}</Text>
+                );
+            }
+            }
+            keyExtractor= {(item) => item.title}
+            />
         </View>
-    );
+    )
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    textStyle: {
+        marginVertical: 30,
+        fontFamily: 'Raleway_200ExtraLight',
+        fontSize: 16
+    }
+});
 
 export default BookList;
+
