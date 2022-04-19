1e00ae57c439de50725fbcc0cf0ddf0b4dcce8a4
Eva S. Poulsen <eva@btech.au.dk>
2022-04-19 21:17:44 +0200

    Fonts1

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,15 +1,24 @@
 import { View, FlatList, Text } from 'react-native';
 import React from 'react';
+import AppLoading from 'expo-app-loading';
 import books from "../../data/books.json";
+import { useFonts, Montserrat_300Light } from '@expo-google-fonts/montserrat';
 
 const BookList = () => {
+    let [fontsLoaded] = useFonts({
+        Montserrat_300Light,
+    });
+    if (!fontsLoaded) {
+        return <AppLoading />
+    }
+
   return (
     <View>
       <FlatList
         data={books}
         renderItem={({ item }) => {
             return (
-                <Text>{item.title}</Text>
+                <Text style={styles.textStyle}>{item.title}</Text>
             );
         }}
         keyExtractor= {(item) => item.title}
@@ -18,5 +27,12 @@ const BookList = () => {
   );
 }
 
+const styles = {
+    textStyle: {
+        marginVertical: 30,
+        fontFamily: 'Montserrat_300Light',
+        fontSize: 16
+    }
+}
 export default BookList;
 
